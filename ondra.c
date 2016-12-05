90b44ee676e7cc045db0dc7e899d09af8eda66e8
Staon <stareko@email.cz>
2016-12-05 10:10:50 +0100

    Some changes

--- ondra.c
+++ ondra.c
@@ -3,6 +3,7 @@
 int main(
     int argc_,
     char *argv_[]) {
+  /* -- nice comment */
   zsock_t *push = zsock_new_push("tcp://192.168.1.107:7777");
   zstr_send(push, "Ondra");
   zclock_sleep(1000);
