af81bb86df128ddce8e6908ae53dfaef3f85e263
Staon <stareko@email.cz>
2016-12-05 11:18:33 +0100

    My version

--- fmq.c
+++ fmq.c
@@ -1,6 +1,24 @@
 #include <czmq.h>
 
+static void makeReady(
+    zmsg_t **msg_,
+    zframe_t* id_) {
+  *msg_ = zmsg_new();
+  zmsg_add(*msg_, id_);
+  zmsg_addstr(*msg_, "READY");
+}
+
+static void makeError(
+    zmsg_t **msg_,
+    zframe_t* id_) {
+  *msg_ = zmsg_new();
+  zmsg_add(*msg_, id_);
+  zmsg_addstr(*msg_, "ERROR");
+}
+
 int main () {
+
+    zsys_init();
 
     zsock_t *server = zsock_new_router ("inproc://fmq");
     zsock_t *client = zsock_new_dealer ("inproc://fmq");
@@ -15,10 +33,27 @@ int main () {
 
     zmsg_t *msg2 = zmsg_recv (server);
     zmsg_print (msg2);
+    zframe_t* routing_id_ = zmsg_pop(msg2);
+    char *command_ = zmsg_popstr(msg2);
+    zsys_info(command_);
+    zmsg_t *response_;
+    if(strcmp(command_, "HELLO") == 0) {
+      makeReady(&response_, routing_id_);
+    }
+    else
+      makeError(&response_, routing_id_);
+    zmsg_send(&response_, server);
+    zstr_free(command_);
 
-    char *command = zmsg_popstr (msg2);
-    zsys_info (command);
-    zstr_free (&command);
+    zmsg_t *msg3_ = zmsg_recv(client);
+    zmsg_print(msg3_);
+    char *id2_ = zmsg_popstr(msg3_);
+    zstr_free(&id2_);
+    char *code_ = zmsg_popstr(msg3_);
+    zsys_info(code_);
+    zstr_free(&code_);
+
+    zstr_free (&command_);
     zmsg_destroy (&msg2);
 
     zsock_destroy (&server);
