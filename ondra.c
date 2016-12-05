750068bb7f51efb96d66ae5b7d6d0a6a15e8a40c
Staon <stareko@email.cz>
2016-12-05 09:59:29 +0100

    First attempt

---
+++ ondra.c
@@ -0,0 +1,12 @@
+#include <czmq.h>
+
+int main(
+    int argc_,
+    char *argv_[]) {
+  zsock_t *push = zsock_new_push("tcp://192.168.1.107:7777");
+  zstr_send(push, "Ondra");
+  zclock_sleep(1000);
+  zsock_destroy(&push);
+
+  return 0;
+}
